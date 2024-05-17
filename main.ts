88fafc8ca2cf933b9bf7670f8e95557fdeef18e7
onaisatahir <168289838+onaisatahir@users.noreply.github.com>
2024-05-17 08:00:25 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,55 +1,44 @@
-//Q2:/
-//let personName="onaisa";
-//console.log('', personnNme,"would you like to learn some typescript'
-//Q3:
-let personName="Onaisa";
-//console.log(personName.toLowerCase);
-//console.log(personName.toUpperCase());
-//console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
-//onsole.log("onaisa\nONAISA\nOnaisa");
-//Q4:
- console.log("Ralph waldo once said" ,'"life is a journey,not  destination"');
-//Q4:
-let famousPerson="Ralph walda";
-//let message="oncesaid,'life is a journey ,not destination'";
-console.log("famousPerson,message");
-//Q6:
- //let whiteSpace="\n\t Onaisa Tahir\t\n"; 
-// console.log(whiteSpace);
-//let withoutwhiteSpace="Onaisa Tahir ".trim(); 
-//console.log(withoutwhiteSpace);
-//Q7,8:
-console.log(4+4);
-console.log(10-2);
-console.log(2*4);
-console.log(16/2);
-//Q9:
-//let favouriteNumber=3;
-//console.log(' My favourite number',favouriteNumber);
-//Q10:
-//
-let whiteSpace="\n\t Onaisa Tahir";
-console.log(whiteSpace);
-let withoutwhiteSpace=whiteSpace.trim();
-console.log(withoutwhiteSpace);
-//Q11:
-//let friendName=["Onaisa","Amna","Tooba","Sabeen"]
-//console.log(friendName[0])
-//console.log(friendName[1])
-//console.log(friendName[2])
-//console.log(friendName[3])
-//Q12:
-let friendname=["Onaisa","Amna","Tooba","Sabeen"]
-let message=["Be work hard for IT course"]
-console.log(friendname[0],message)
-console.log(friendname[1],message)
-console.log(friendname[2],message)
-console.log(friendname[3],message)
+import inquirer from "inquirer"; 
+import chalk from "chalk";
 
+//step 01
+const answer :{
+    numberOne: number,
+    numberTwo: number,
+    opertor:  string
+}  = await inquirer.prompt([
+ {  type: "number",
+    name: "numberOne",
+    message: "Enter your first number"
+ },
+ {  type: "number",
+    name: "numberTwo",
+    message: "Enter your second number"
+ },
+ {  type: "list",
+    name: "opertor",
+    message: "Enter your opertor",
+    choices: ["+" ,"-", "*", "/"]}
+])
+//step 02
+const { numberOne , numberTwo ,opertor }=answer;
 
+let result 
+ switch (opertor) {
 
-
-
-
-
-
+   case"+" :
+       result = numberOne + numberTwo;
+       break;         
+   case"-" :
+        result = numberOne - numberTwo;
+        break;
+   case"*" :
+        result =numberOne * numberOne;
+        break;
+   case"/" :
+        result = numberOne / numberTwo;
+    default:
+      console.log("Invalid opertor");
+ }    
+  console.log('${nunberOne} ${opertor} ${nunmberTwo}  =[${result}');
+console.log(6 + 5);
